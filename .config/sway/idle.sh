d4964731ad1e9c68b16512205201a5fe7efa482e
Thomas Faschang <thomasfaschang@mailbox.org>
2022-09-16 16:28:50 +0200

    added i3 config and updated sway to pride colors

--- .config/sway/idle.sh
+++ .config/sway/idle.sh
@@ -1,5 +1,14 @@
-swayidle \
-	timeout 600 '~/.config/sway/lock.sh --grace 30' \
-	timeout 660 'swaymsg "output * dpms off"' \
-	resume 'swaymsg "output * dpms on"' \
-	before-sleep '~/.config/sway/lock.sh'
+OUTPUT=$(iwgetid)
+HOME='Meranien'
+if [[ "$OUTPUT" == *"$HOME"* ]];
+then
+  swayidle \
+	  timeout 600 'swaymsg "output * dpms off"' \
+	  resume 'swaymsg "output * dpms on"'
+else
+  swiayidle \
+	  timeout 600 '~/.config/sway/lock.sh --grace 30' \
+	  timeout 660 'swaymsg "output * dpms off"' \
+	  resume 'swaymsg "output * dpms on"' \
+	  before-sleep '~/.config/sway/lock.sh'
+fi
